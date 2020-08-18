32dda5aecfeade4c5aa44e456a924245e68c5a64
wangjiawei601996 <wangjiawei601996@pwrd.com>
2020-08-18 18:13:45 +0800

    feat: 新增检索功能

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -50,44 +50,98 @@ struct ContentView: View {
     @State private var uitemData: [ItemModel] = cload()
     @State var showCodeInput: Bool = false
     @State private var currentData: ItemModel?
+    @State private var searchData: [ItemModel] = []
+    @State private var searchText: String = ""
     
     /// 定制左侧列表视图
     var list: some View {
         // 默认应该使用数据读取，暂时模拟
-        List {
-            ForEach(self.uitemData, id: \.self) { item in
-                #if os(iOS)
-                
-                NavigationLink(
-                    destination: GroupView(title: item.titleName, content: {
-                        DetailView(sitem: &item)
-                            .navigationTitle(item.titleName)
-                    }),
-                    label: {
-                        #if os(iOS)
-                        Label(title, systemImage: icon)
-                            .font(.headline)
-                            .padding(.vertical, 8)
-                        #else
-                        Label(item.titleName, systemImage: "capsule")
-                        #endif
+        VStack {
+            TextField("模糊搜索", text: self.$searchText)
+                // 可以使用reduce集合上层信号结果
+                .onReceive(self.searchText.publisher.reduce("", {t,c in t + String(c) })){ some in
+                // 原代码内容不变
+                    self.searchData = self.uitemData.compactMap({ item in
+                        if item.titleName.hasPrefix(some) {
+                            return item
+                        }
+                        return nil
                     })
-                
-                #else
-                Button(item.titleName) {
-                    print("选中\(item.code)")
-                    self.currentData = item
                 }
-                #endif
+                .frame(maxWidth: .infinity)
+            List {
                 
-                
-                
-                /*
-                Grouping(title: item.titleName, icon: "capsule") {
-                    DetailView(iconName: "tv.circle.fill", desc: item.desc, code: item.code, linkUrl: "https://www.baidu.com")
+                ForEach(self.searchData.count > 0 ? self.searchData : self.uitemData, id: \.self) { item in
+                    #if os(iOS)
+                    
+                    NavigationLink(
+                        destination: GroupView(title: item.titleName, content: {
+                            DetailView(sitem: &item)
+                                .navigationTitle(item.titleName)
+                        }),
+                        label: {
+                            #if os(iOS)
+                            Label(title, systemImage: icon)
+                                .font(.headline)
+                                .padding(.vertical, 8)
+                            #else
+                            Label(item.titleName, systemImage: "capsule")
+                            #endif
+                        })
+                    
+                    #else
+                    
+                    Button(action: {
+                        self.currentData = item
+                    }, label: {
+                        HStack {
+                            let iconName = self.currentData == item ? "macmini.fill" : "macmini"
+                            Image(systemName: iconName)
+                            Text(item.titleName)
+                                .bold()
+                                .foregroundColor(.black)
+                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        }
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    })
+                    .background(self.currentData == item ? Color.yellow : Color.clear)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    
+                    /*
+                    ZStack {
+                        HStack {
+                            let iconName = self.currentData == item ? "macmini.fill" : "macmini"
+                            Image(systemName: iconName)
+                            Text(item.titleName)
+                                .bold()
+                                .foregroundColor(.yellow)
+                                .frame(width: 80, height: 40, alignment: .trailing)
+                                .frame(maxWidth: .infinity)
+    //                            .frame(height: 40, alignment: .trailing, maxWidth: .infinity)
+    //                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        }
+                        .background(self.currentData == item ? Color.green : Color.clear)
+                    }
+                    .onTapGesture(count: 1, perform: {
+                        print("选中\(item.code)")
+                        self.currentData = item
+                    })
+                    .contentShape(Rectangle()) // fix:
+                    .padding(.all, 5)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+     */
+                    
+                    #endif
+                    
+                    
+                    
+                    /*
+                    Grouping(title: item.titleName, icon: "capsule") {
+                        DetailView(iconName: "tv.circle.fill", desc: item.desc, code: item.code, linkUrl: "https://www.baidu.com")
+                    }
+                    */
+                    
                 }
-                */
-                
             }
         }
     }
@@ -98,12 +152,12 @@ struct ContentView: View {
             list.navigationTitle("WMUIKit")
             Text("选择一个视图")
             #elseif os(OSX)
-            list // .listStyle(SidebarListStyle())
+            list.listStyle(SidebarListStyle())
             DetailView(sitem: self.$currentData)
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
             #endif
         }
-        .navigationTitle("代码拷贝神器")
+        .navigationTitle((self.currentData?.titleName == nil ? "代码拷贝" : self.currentData?.titleName) ?? "代码拷贝")
         .accentColor(.accentColor)
         .toolbar(content: {
             HStack {
@@ -116,6 +170,15 @@ struct ContentView: View {
                     CodeInput(uitemData: $uitemData, showCodeInput: $showCodeInput)
                 }
                 Link("查看项目地址", destination: URL(string: "https://github.com/wang542413041/awesomeui")!)
+                Button("") {
+                    self.searchData = self.uitemData.compactMap({ item in
+                        if item.titleName.hasPrefix("模拟") {
+                            return item
+                        }
+                        return nil
+                    })
+                }
+
             }.frame(alignment: .center)
         })
         // .navigationSubtitle("内部UI代码示例")
